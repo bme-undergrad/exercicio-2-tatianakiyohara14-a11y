e13f635d48205bd7236cfa0b04f554460ce8d3c3
tatianakiyohara14-a11y <tatianakiyohara14@gmail.com>
2025-09-13 16:07:22 -0300

    Update exercicio2.m

--- exercicio2.m
+++ exercicio2.m
@@ -12,11 +12,7 @@ pkg load optim
 % C = 10*blendA + (2*NG)*blendB  --> funaoo objetivo do custo
 f = [10, NG*2]
 
-% 0.30*blendA + 0.20*blendB = 15 --> requisito de polietileno
-% 0.20*blendA + 0.25*blendB = 10 --> resquisito de PLA
-% 0.25*blendA + 0.30*blendB = 12 --> requisito de polipropileno
-
-% restrioees de desigualdade A.x <= b
+% restrioes de desigualdade A.x <= b
 A = [-0.30, -0.20;
     -0.20, -0.25;
     -0.25, -0.30];
@@ -33,7 +29,7 @@ lb = [0;0];
 ub = [20;50];
 
 x = linprog(f, A, b, Aeq, beq, lb, ub)
-x =x'
+x = x'
 
 % mantenha essas duas linhas finais
 blendA = x(1);
